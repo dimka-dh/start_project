f886ba27fbb90528dd21b6190d8b6c6ee33cc963
dh <dimm15@mail.ru>
2024-09-30 14:52:49 +0300

    выполненное задание 1_2

---
+++ Module_1/module_1_2.py
@@ -0,0 +1,7 @@
+dz = 12
+count_hours = 1.5
+name_course = 'Python'
+time_1_lesson =count_hours/dz
+print(time_1_lesson)
+#print(first_name, 'дал', second_name, apple_count, 'яблок и', orange_count, 'апельсинов')
+print('Python, всего задач:', dz, ', затрачено часов:', count_hours, ', среднее время выполнения', time_1_lesson, 'часа.')
