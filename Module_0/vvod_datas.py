8c535e1b9c8e36fb9552c891f0dff45ca8656981
dh <dimm15@mail.ru>
2024-09-30 14:42:02 +0300

    init (модуль 0)

---
+++ Module_0/vvod_datas.py
@@ -0,0 +1,13 @@
+#"1st program"
+print((9**0.5)*5)
+#"2nd program"
+print(9.99>9.98 and 1000!=100.1)
+#"3rd program"
+one=2*2+2
+print(one)
+two=2*(2+2)
+print(two)
+print(one==two)
+#"4th program"
+aaa=float('123.456')
+print(int(aaa*10)%5)
