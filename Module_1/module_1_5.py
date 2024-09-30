b8fe713ae072bd10bb41a12c503447f6d8f546a2
dh <dimm15@mail.ru>
2024-09-30 14:53:05 +0300

    выполненное задание 1_5

---
+++ Module_1/module_1_5.py
@@ -0,0 +1,6 @@
+imumutable_var=123,'word',True,4.56
+print(imumutable_var)
+#кортеж – это неизменяемая упорядоченная коллекция, можно, например, менять элементы списка, находящегося внутри кортежа.
+mutable_list=['yandex', 'worse', 'google', '?']
+mutable_list[1]='better'
+print(mutable_list[0:3])
