fa71776bf1bf41ebe1cdf932947d9d3eff528e19
dh <dimm15@mail.ru>
2024-10-06 16:17:54 +0300

    задание_3_5

---
+++ Module_3/Module_3_5.py
@@ -0,0 +1,12 @@
+def get_multiplied_digits(number):
+    str_number = str(number)
+    first = int(str_number[0])
+    if len(str_number) > 1:
+        return first * get_multiplied_digits(int(str_number[1:]))
+    else:
+        return first
+
+result = get_multiplied_digits(40203)
+result2 = get_multiplied_digits(5081)
+print(result)
+print(result2)
