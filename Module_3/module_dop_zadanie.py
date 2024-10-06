072501fe52d81e85e2c99cae47163de01da2a2ba
dh <dimm15@mail.ru>
2024-10-06 17:31:25 +0300

    доп задание модуль 3

---
+++ Module_3/module_dop_zadanie.py
@@ -0,0 +1,26 @@
+def calculate_structure_sum(data):
+    total_sum = 0
+
+    for element in data:
+        if isinstance(element, int):
+            total_sum += element
+        elif isinstance(element, str):
+            total_sum += len(element)
+        elif isinstance(element, (list, tuple, set)):
+            total_sum += calculate_structure_sum(element)
+        elif isinstance(element, dict):
+            total_sum += calculate_structure_sum(element.keys())
+            total_sum += calculate_structure_sum(element.values())
+    return total_sum
+
+
+data_structure = [
+    [1, 2, 3],
+    {'a': 4, 'b': 5},
+    (6, {'cube': 7, 'drum': 8}),
+    "Hello",
+    ((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+
+result = calculate_structure_sum(data_structure)
+print(result)
