603f241f42f8f15d20c9dfb942ea2577a66e7251
dh <dimm15@mail.ru>
2024-10-07 10:44:49 +0300

    задание_3_5 добавлено сравнение на 0 в конце числа.

--- Module_3/module_3_5.py
+++ Module_3/module_3_5.py
@@ -1,5 +1,7 @@
 def get_multiplied_digits(number):
     str_number = str(number)
+    if len(str_number) == 1:
+        return int(str_number) if str_number != '0' else 1
     first = int(str_number[0])
     if len(str_number) > 1:
         return first * get_multiplied_digits(int(str_number[1:]))
@@ -7,6 +9,6 @@ def get_multiplied_digits(number):
         return first
 
 result = get_multiplied_digits(40203)
-result2 = get_multiplied_digits(5081)
+result2 = get_multiplied_digits(50810)
 print(result)
 print(result2)
