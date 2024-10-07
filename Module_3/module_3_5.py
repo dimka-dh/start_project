bf70c280e042ddbe6dc7aa4060545983e999dc1a
dh <dimm15@mail.ru>
2024-10-07 10:55:14 +0300

    задание_3_5 добавлено сравнение на 0 в конце числа. добавлен пропуск умножения на 0, проверка если число из 1 символа

--- Module_3/module_3_5.py
+++ Module_3/module_3_5.py
@@ -3,10 +3,10 @@ def get_multiplied_digits(number):
     if len(str_number) == 1:
         return int(str_number) if str_number != '0' else 1
     first = int(str_number[0])
-    if len(str_number) > 1:
-        return first * get_multiplied_digits(int(str_number[1:]))
+    if first == 0:
+        return get_multiplied_digits(int(str_number[1:]))
     else:
-        return first
+        return first * get_multiplied_digits(int(str_number[1:]))
 
 result = get_multiplied_digits(40203)
 result2 = get_multiplied_digits(50810)
