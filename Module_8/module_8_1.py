9f5eddbe78cd2efb8d3c0561724ba51726bcda47
dh <dimm15@mail.ru>
2024-11-06 21:40:55 +0300

    задание_8_1_комментарии+замечания

--- Module_8/module_8_1.py
+++ Module_8/module_8_1.py
@@ -17,3 +17,11 @@ def add_everything_up(a, b):
 print(add_everything_up(123.456, 'строка'))  # 123.456строка
 print(add_everything_up('яблоко', 4215))     # яблоко4215
 print(add_everything_up(123.456, 7))         # 130.456
+
+
+# Вы отлично справились с решением задачи. Однако isinstance в блоке try немного бесмыслен. Вот рабочий код без него:
+# def add_everything_up(a, b):
+#   try:
+#       return a + b
+#   except TypeError:
+#       return f"{a}{b}"
