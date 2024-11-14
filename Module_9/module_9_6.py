6a158773c3f4268161380a953e2711a5ebfea49e
dh <dimm15@mail.ru>
2024-11-14 19:00:21 +0300

    задание_9_6 изменен порядок циклов

--- Module_9/module_9_6.py
+++ Module_9/module_9_6.py
@@ -1,7 +1,7 @@
 def all_variants(text):
-    for start in range(len(text)):
-        for end in range(start + 1, len(text) + 1):
-            yield text[start:end]
+    for length in range(1, len(text) + 1):
+        for start in range(len(text) - length + 1):
+            yield text[start:start + length]
 
 a = all_variants("abc")
 for i in a:
