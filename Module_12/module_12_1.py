8f570282cb153fb8dbc156f5c9d983f3de8112c0
dh <dimm15@mail.ru>
2024-11-26 17:55:20 +0300

    задание_12_1

---
+++ Module_12/module_12_1.py
@@ -0,0 +1,30 @@
+import unittest
+from runner import Runner
+
+
+class RunnerTest(unittest.TestCase):
+    def test_walk(self):
+        runner = Runner("Walker")
+        for i in range(10):
+            runner.walk()
+        self.assertEqual(runner.distance, 50)
+
+    def test_run(self):
+        runner = Runner("Runner")
+        for i in range(10):
+            runner.run()
+        self.assertEqual(runner.distance, 100)
+
+    def test_challenge(self):
+        runner1 = Runner("Runner1")
+        runner2 = Runner("Runner2")
+
+        for i in range(10):
+            runner1.run()
+            runner2.walk()
+
+        self.assertNotEqual(runner1.distance, runner2.distance)
+
+
+if __name__ == "__main__":
+    unittest.main()
