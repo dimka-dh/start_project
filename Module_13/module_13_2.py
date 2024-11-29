fe6032fd56362fbf0f16e7091e3b58b22ea95996
dh <dimm15@mail.ru>
2024-11-29 17:52:14 +0300

    задание_13_2_по ТЗ

--- Module_13/module_13_2.py
+++ Module_13/module_13_2.py
@@ -10,13 +10,11 @@ dp = Dispatcher(bot)
 @dp.message_handler(commands=["start"])
 async def start(message):
     print("Привет! Я бот помогающий твоему здоровью.")
-    await message.reply("Привет! Я бот помогающий твоему здоровью.")
 
 
 @dp.message_handler()
 async def all_messages(message):
     print("Введите команду /start, чтобы начать общение.")
-    await message.reply("Введите команду /start, чтобы начать общение.")
 
 
 if __name__ == "__main__":
