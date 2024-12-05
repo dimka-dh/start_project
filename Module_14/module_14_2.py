54debde8dd9bed5e04de9008e8d2e916ae225bc3
dh <dimm15@mail.ru>
2024-12-05 15:59:28 +0300

    задание_14_2

---
+++ Module_14/module_14_2.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+conn = sqlite3.connect("not_telegram.db")
+cursor = conn.cursor()
+
+cursor.execute("DELETE FROM Users WHERE id = 6")
+conn.commit()
+
+cursor.execute("SELECT COUNT(*) FROM Users")
+total_users = cursor.fetchone()[0]
+
+cursor.execute("SELECT SUM(balance) FROM Users")
+all_balances = cursor.fetchone()[0]
+
+average_balance = all_balances / total_users if total_users > 0 else 0
+
+print(average_balance)
+
+conn.close()
