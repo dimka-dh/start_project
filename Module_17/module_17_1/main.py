9ef65d950650a91320d87d235125c0a032c88ffa
dh <dimm15@mail.ru>
2024-12-20 16:24:53 +0300

    задание_17_1_change

--- Module_17/module_17_1/main.py
+++ Module_17/module_17_1/main.py
@@ -1,12 +1,33 @@
+from fastapi import FastAPI, HTTPException, Path, Request
+from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
+from typing import List, Annotated
 from fastapi import FastAPI
 from routers import task, user
 
 app = FastAPI()
 
-app.include_router(task.router)
-app.include_router(user.router)
+
+class User(BaseModel):
+    id: int
+    username: str
+    age: int
+
+
+users: List[User] = []
+
+templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/")
 def root():
     return {"message": "Welcome to Taskmanager"}
+
+
+app.include_router(task.router)
+app.include_router(user.router)
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("main:app", reload=True)
